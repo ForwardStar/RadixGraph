eff5ce879a1467da87a9a584ec61f6c34f2279a1
ForwardStar <haoxuanxie@link.cuhk.edu.cn>
2024-10-28 10:18:54 +0000

    Bug fixes

--- src/main.cpp
+++ src/main.cpp
@@ -28,46 +28,29 @@ std::vector<uint64_t> spruce_bfs(SpruceTransVer& spruce, int n, uint64_t src) {
 int main() {
     std::ios::sync_with_stdio(false);
     srand((int)time(NULL));
-    // std::vector<int> d = {8, 8, 8, 8};
-    // std::vector<std::vector<int>> a = {
-    //     {12, 3, 3, 3, 3, 3, 3, 2},
-    //     {16, 3, 3, 2, 2, 2, 2, 2},
-    //     {18, 2, 2, 2, 2, 2, 2, 2},
-    //     {21, 2, 2, 2, 2, 1, 1, 1}
-    // };
     std::vector<int> d = {4, 4, 4, 4};
     std::vector<std::vector<int>> a = {
-        {15, 6, 6, 5},
-        {18, 5, 5, 4},
-        {20, 4, 4, 4},
-        {23, 3, 3, 3}
+        {17, 8, 8, 7},
+        {20, 7, 7, 6},
+        {22, 6, 6, 6},
+        {25, 5, 5, 5}
     };
     int m = 2560000;
     int num_trials = 5;
     
     std::default_random_engine generator;
-    unsigned long long maximum = (1ull << 32) - 1;
+    unsigned long long maximum = (1ull << 40) - 1;
     std::uniform_int_distribution distribution(0ull, maximum);
     int now = 0;
     for (int n = 1000; n <= 1000000; n *= 10) {
-        double duration_insert_edge_adjlinkedlist = 0;
-        double duration_insert_edge_adjarraylist = 0;
         double duration_insert_edge_fstar = 0;
         double duration_insert_edge_spruce = 0;
-        double duration_delete_edge_adjlinkedlist = 0;
-        double duration_delete_edge_adjarraylist = 0;
         double duration_delete_edge_fstar = 0;
         double duration_delete_edge_spruce = 0;
-        double duration_update_edge_adjlinkedlist = 0;
-        double duration_update_edge_adjarraylist = 0;
         double duration_update_edge_fstar = 0;
         double duration_update_edge_spruce = 0;
-        double duration_get_neighbours_adjlinkedlist = 0;
-        double duration_get_neighbours_adjarraylist = 0;
         double duration_get_neighbours_fstar = 0;
         double duration_get_neighbours_spruce = 0;
-        double duration_bfs_adjlinkedlist = 0;
-        double duration_bfs_adjarraylist = 0;
         double duration_bfs_fstar = 0;
         double duration_bfs_spruce = 0;
         
@@ -83,8 +66,6 @@ int main() {
             vertex_set.insert(id);
         }
         for (int i = 0; i < num_trials; i++) {
-            AdjacencyLinkedList G_adj_linked;
-            AdjacencyArrayList G_adj_array;
             ForwardStar G_fstar(d[now], a[now]);
             SpruceTransVer spruce;
             std::vector<std::pair<std::pair<uint64_t, uint64_t>, double>> edges;
@@ -93,41 +74,19 @@ int main() {
             {
                 uint64_t u, v;
                 double w;
-                for (int i = 0; i < n - 1; i++) {
-                    edges.push_back(std::make_pair(std::make_pair(vertex_ids[i], vertex_ids[i + 1]), 0.5));
-                }
-                for (int i = 0; i < m - (n - 1); i++) {
+                for (int i = 0; i < m; i++) {
                     int id1 = rand() % n, id2 = rand() % n;
-                    while (id2 == id1 + 1) {
-                        id1 = rand() % n, id2 = rand() % n;
-                    }
                     u = vertex_ids[id1];
                     v = vertex_ids[id2];
                     edges.push_back(std::make_pair(std::make_pair(u, v), 0.5));
                 }
 
-                // auto start = std::chrono::high_resolution_clock::now();
-                // for (auto e : edges) {
-                //     G_adj_linked.InsertEdge(e.first.first, e.first.second, e.second);
-                // }
-                // auto end = std::chrono::high_resolution_clock::now();
-                // std::chrono::duration<double> duration = end - start;
-                // duration_insert_edge_adjlinkedlist += duration.count();
-
                 auto start = std::chrono::high_resolution_clock::now();
-                for (auto e : edges) {
-                    G_adj_array.InsertEdge(e.first.first, e.first.second, e.second);
-                }
-                auto end = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> duration = end - start;
-                duration_insert_edge_adjarraylist += duration.count();
-
-                start = std::chrono::high_resolution_clock::now();
                 for (auto e : edges) {
                     G_fstar.InsertEdge(e.first.first, e.first.second, e.second);
                 }
-                end = std::chrono::high_resolution_clock::now();
-                duration = end - start;
+                auto end = std::chrono::high_resolution_clock::now();
+                std::chrono::duration<double> duration = end - start;
                 duration_insert_edge_fstar += duration.count();
                 
                 start = std::chrono::high_resolution_clock::now();
@@ -149,28 +108,12 @@ int main() {
                     edges_update.push_back(e);
                 }
 
-                // auto start = std::chrono::high_resolution_clock::now();
-                // for (auto e : edges_update) {
-                //     G_adj_linked.UpdateEdge(e.first.first, e.first.second, e.second);
-                // }
-                // auto end = std::chrono::high_resolution_clock::now();
-                // std::chrono::duration<double> duration = end - start;
-                // duration_update_edge_adjlinkedlist += duration.count();
-
                 auto start = std::chrono::high_resolution_clock::now();
-                for (auto e : edges_update) {
-                    G_adj_array.UpdateEdge(e.first.first, e.first.second, e.second);
-                }
-                auto end = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> duration = end - start;
-                duration_update_edge_adjarraylist += duration.count();
-
-                start = std::chrono::high_resolution_clock::now();
                 for (auto e : edges_update) {
                     G_fstar.UpdateEdge(e.first.first, e.first.second, e.second);
                 }
-                end = std::chrono::high_resolution_clock::now();
-                duration = end - start;
+                auto end = std::chrono::high_resolution_clock::now();
+                std::chrono::duration<double> duration = end - start;
                 duration_update_edge_fstar += duration.count();
                 
                 start = std::chrono::high_resolution_clock::now();
@@ -190,7 +133,7 @@ int main() {
                 std::unordered_set<int> idx_delete;
                 for (int i = 0; i < 1000; i++) {
                     auto id = rand() % m;
-                    while (idx_delete.find(id) != idx_delete.end() || id < n - 1) {
+                    while (idx_delete.find(id) != idx_delete.end()) {
                         id = rand() % m;
                     }
                     idx_delete.insert(id);
@@ -198,28 +141,12 @@ int main() {
                     edges_delete.push_back(e);
                 }
 
-                // auto start = std::chrono::high_resolution_clock::now();
-                // for (auto e : edges_delete) {
-                //     G_adj_linked.DeleteEdge(e.first.first, e.first.second);
-                // }
-                // auto end = std::chrono::high_resolution_clock::now();
-                // std::chrono::duration<double> duration = end - start;
-                // duration_delete_edge_adjlinkedlist += duration.count();
-
                 auto start = std::chrono::high_resolution_clock::now();
-                for (auto e : edges_delete) {
-                    G_adj_array.DeleteEdge(e.first.first, e.first.second);
-                }
-                auto end = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> duration = end - start;
-                duration_delete_edge_adjarraylist += duration.count();
-
-                start = std::chrono::high_resolution_clock::now();
                 for (auto e : edges_delete) {
                     G_fstar.DeleteEdge(e.first.first, e.first.second);
                 }
-                end = std::chrono::high_resolution_clock::now();
-                duration = end - start;
+                auto end = std::chrono::high_resolution_clock::now();
+                std::chrono::duration<double> duration = end - start;
                 duration_delete_edge_fstar += duration.count();
                 
                 start = std::chrono::high_resolution_clock::now();
@@ -235,31 +162,13 @@ int main() {
 
             // Get neighbours
             {
-                // auto start = std::chrono::high_resolution_clock::now();
-                // for (int i = 0; i < n; i++) {
-                //     std::vector<AdjacencyLinkedList::WeightedEdge*> neighbours;
-                //     G_adj_linked.GetNeighbours(vertex_ids[i], neighbours);
-                // }
-                // auto end = std::chrono::high_resolution_clock::now();
-                // std::chrono::duration<double> duration = end - start;
-                // duration_get_neighbours_adjlinkedlist += duration.count();
-
                 auto start = std::chrono::high_resolution_clock::now();
-                for (int i = 0; i < n; i++) {
-                    std::vector<AdjacencyArrayList::WeightedEdge*> neighbours;
-                    G_adj_array.GetNeighbours(vertex_ids[i], neighbours);
-                }
-                auto end = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> duration = end - start;
-                duration_get_neighbours_adjarraylist += duration.count();
-
-                start = std::chrono::high_resolution_clock::now();
                 for (int i = 0; i < n; i++) {
                     std::vector<ForwardStar::WeightedEdge*> neighbours;
                     G_fstar.GetNeighbours(vertex_ids[i], neighbours);
                 }
-                end = std::chrono::high_resolution_clock::now();
-                duration = end - start;
+                auto end = std::chrono::high_resolution_clock::now();
+                std::chrono::duration<double> duration = end - start;
                 duration_get_neighbours_fstar += duration.count();
 
                 start = std::chrono::high_resolution_clock::now();
@@ -276,22 +185,10 @@ int main() {
 
             // BFS
             {
-                // auto start = std::chrono::high_resolution_clock::now();
-                // G_adj_linked.BFS(vertex_ids[0]);
-                // auto end = std::chrono::high_resolution_clock::now();
-                // std::chrono::duration<double> duration = end - start;
-                // duration_bfs_adjlinkedlist += duration.count();
-
                 auto start = std::chrono::high_resolution_clock::now();
-                G_adj_array.BFS(vertex_ids[0]);
+                auto res_fstar = G_fstar.BFS(vertex_ids[0]);
                 auto end = std::chrono::high_resolution_clock::now();
                 std::chrono::duration<double> duration = end - start;
-                duration_bfs_adjarraylist += duration.count();
-
-                start = std::chrono::high_resolution_clock::now();
-                auto res_fstar = G_fstar.BFS(vertex_ids[0]);
-                end = std::chrono::high_resolution_clock::now();
-                duration = end - start;
                 duration_bfs_fstar += duration.count();
 
                 start = std::chrono::high_resolution_clock::now();
@@ -311,28 +208,18 @@ int main() {
         }
         ++now;
 
-        // std::cout << "Average insertion time for adjacency linked list: " << duration_insert_edge_adjlinkedlist /  << "s" << std::endl;
-        std::cout << "Average insertion time for adjacency array list: " << duration_insert_edge_adjarraylist / num_trials << "s" << std::endl;
         std::cout << "Average insertion time for forward star: " << duration_insert_edge_fstar / num_trials << "s" << std::endl;
         std::cout << "Average insertion time for spruce: " << duration_insert_edge_spruce / num_trials << "s" << std::endl;
         std::cout << std::endl;
-        // std::cout << "Average deletion time for adjacency linked list: " << duration_delete_edge_adjlinkedlist / num_trials << "s" << std::endl;
-        std::cout << "Average deletion time for adjacency array list: " << duration_delete_edge_adjarraylist / num_trials << "s" << std::endl;
         std::cout << "Average deletion time for forward star: " << duration_delete_edge_fstar / num_trials << "s" << std::endl;
         std::cout << "Average deletion time for spruce: " << duration_delete_edge_spruce / num_trials << "s" << std::endl;
         std::cout << std::endl;
-        // std::cout << "Average update time for adjacency linked list: " << duration_update_edge_adjlinkedlist / num_trials << "s" << std::endl;
-        std::cout << "Average update time for adjacency array list: " << duration_update_edge_adjarraylist / num_trials << "s" << std::endl;
         std::cout << "Average update time for forward star: " << duration_update_edge_fstar / num_trials << "s" << std::endl;
         std::cout << "Average update time for spruce: " << duration_update_edge_spruce / num_trials << "s" << std::endl;
         std::cout << std::endl;
-        // std::cout << "Average get neighbours time for adjacency linked list: " << duration_get_neighbours_adjlinkedlist / num_trials << "s" << std::endl;
-        std::cout << "Average get neighbours time for adjacency array list: " << duration_get_neighbours_adjarraylist / num_trials << "s" << std::endl;
         std::cout << "Average get neighbours time for forward star: " << duration_get_neighbours_fstar / num_trials << "s" << std::endl;
         std::cout << "Average get neighbours time for spruce: " << duration_get_neighbours_spruce / num_trials << "s" << std::endl;
         std::cout << std::endl;
-        // std::cout << "Average BFS time for adjacency linked list: " << duration_bfs_adjlinkedlist / num_trials << "s" << std::endl;
-        std::cout << "Average BFS time for adjacency array list: " << duration_bfs_adjarraylist / num_trials << "s" << std::endl;
         std::cout << "Average BFS time for forward star: " << duration_bfs_fstar / num_trials << "s" << std::endl;
         std::cout << "Average BFS time for spruce: " << duration_bfs_spruce / num_trials << "s" << std::endl;
     }
