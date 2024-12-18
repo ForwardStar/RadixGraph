87d46ca72c8817e2ea64d7b1d2dbfc4be74679b9
ForwardStar <haoxuanxie@link.cuhk.edu.cn>
2024-12-18 08:51:45 +0000

    Bug fixes

--- src/forward_star.h
+++ src/forward_star.h
@@ -22,7 +22,7 @@ class ForwardStar {
 
         Trie* vertex_index = nullptr;
         std::atomic<int> num_dummy_nodes, cnt;
-        tbb::concurrent_vector<int> thread_pool;
+        std::vector<int> thread_pool;
         std::vector<DummyNode*> dummy_nodes;
         std::mutex mtx;
 
