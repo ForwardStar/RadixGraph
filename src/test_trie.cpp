475ec454a6bf6644282b0e6c77e574fdac004c41
ForwardStar <119010351@link.cuhk.edu.cn>
2025-06-23 16:23:07 +0800

    Add testing for trie

--- src/test_trie.cpp
+++ src/test_trie.cpp
@@ -51,17 +51,44 @@ int main() {
     for (auto u : vertex_ids) {
         vids.emplace_back(u);
     }
-    #pragma omp parallel for num_threads(10)
+    double duration_trie_base_insert = 0;
+    double duration_trie_base_query = 0;
+    double duration_trie_opt_insert = 0;
+    double duration_trie_opt_query = 0;
+    // #pragma omp parallel for num_threads(10)
     for (int i = 0; i < n; i++) {
         uint64_t id = vids[i];
+        auto start = std::chrono::high_resolution_clock::now();
         auto x = trie_base.RetrieveVertex(id, true);
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end - start;
+        duration_trie_base_insert += duration.count();
+
+        start = std::chrono::high_resolution_clock::now();
         x = trie_opt.RetrieveVertex(id, true);
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+        duration_trie_opt_insert += duration.count();
+
+        start = std::chrono::high_resolution_clock::now();
         auto tmp = trie_base.RetrieveVertex(id);
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+        duration_trie_base_query += duration.count();
         assert(tmp->node == id);
+        
+        start = std::chrono::high_resolution_clock::now();
         tmp = trie_opt.RetrieveVertex(id);
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+        duration_trie_opt_query += duration.count();
         assert(tmp->node == id);
     }
     std::cout << "Allocated space of a baseline Trie: " << trie_base.size() << std::endl;
     std::cout << "Allocated space of your Trie: " << trie_opt.size() << std::endl;
+    std::cout << "Total insertion time of a baseline Trie: " << duration_trie_base_insert << std::endl;
+    std::cout << "Total insertion time of your Trie: " << duration_trie_opt_insert << std::endl;
+    std::cout << "Total query time of a baseline Trie: " << duration_trie_base_query << std::endl;
+    std::cout << "Total query time of your Trie: " << duration_trie_opt_query << std::endl;
     return 0;
 }
