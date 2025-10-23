813196523d15e4e0da2b8e12db57fb414ef2dbbd
ForwardStar <61535734+ForwardStar@users.noreply.github.com>
2025-10-23 20:18:42 +0800

    Bug fix

--- examples/single-thread.cpp
+++ examples/single-thread.cpp
@@ -10,18 +10,18 @@ int main() {
     int total_weight = 0;
     for (NodeID i = 0; i < 32; i++) {
         std::vector<WeightedEdge> neighbors;
-        G.GetNeighbors(i, neighbors); // Get neighbors
+        G.GetNeighbours(i, neighbors); // Get neighbors
         for (auto e : neighbors) total_weight += e.weight;
     }
     assert(total_weight == 15); // total weight is 15
     G.DeleteEdge(0, 1); // Delete edge: from 0 to 1
     G.InsertEdge(0, 5, 1);
     std::vector<WeightedEdge> neighbors;
-    G.GetNeighbors(0, neighbors);
+    G.GetNeighbours(0, neighbors);
     // The only edge from 0 points to 5 with weight 1
     assert(neighbors.size() == 1);
     assert(neighbors[0].weight == 1);
-    auto logical_id = neighbors[0].des; // Internal vertex ID
+    auto logical_id = neighbors[0].idx; // Internal vertex ID
     assert(G.vertex_index->vertex_table[logical_id].node == 5); // Gets the actual ID
     return 0;
 }
